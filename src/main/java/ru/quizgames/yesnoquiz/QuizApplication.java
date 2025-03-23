5b4e6931fd5b789c03273dda5dfaf75b6fb614f5
aleksandr <felleet@gmail.com>
2025-03-24 00:37:34 +0300

    [m] добавил загрузку вопросов

--- src/main/java/ru/quizgames/yesnoquiz/QuizApplication.java
+++ src/main/java/ru/quizgames/yesnoquiz/QuizApplication.java
@@ -11,7 +11,9 @@ public class QuizApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(QuizApplication.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 620, 540);
+        QuizController controller = fxmlLoader.getController();
+        controller.setPrimaryStage(stage);
         stage.setTitle("Викторина");
         stage.setScene(scene);
         stage.show();
--- src/main/java/ru/quizgames/yesnoquiz/QuizController.java
+++ src/main/java/ru/quizgames/yesnoquiz/QuizController.java
@@ -1,14 +1,58 @@
 package ru.quizgames.yesnoquiz;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class QuizController {
     @FXML
+    public Button startButton;
+    @FXML
+    public Button loadButton;
+    @FXML
     private Label welcomeText;
+    FileChooser fileChooser = new FileChooser();
+    private Window primaryStage;
+    private List<Question> questions = new ArrayList<>();
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private void initialize() {
+        startButton.setDisable(questions.isEmpty());
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("CSV files", "*.csv"));
+    }
+
+    @FXML
+    protected void onLoadQuestionsButtonClick() {
+        File selectedFile = fileChooser.showOpenDialog(primaryStage);
+        System.out.printf("File selected: %s%n", selectedFile);
+        try {
+            questions = new QuestionsParser().parse(selectedFile.toPath());
+            System.out.println("Loaded questions: " + questions);
+            welcomeText.setText("Загружено вопросов: " + questions.size());
+            startButton.setDisable(questions.isEmpty());
+        } catch (IOException e) {
+            System.out.println("Failed to load questions: " + e);
+            welcomeText.setText("Ошибка при загрузке вопросов.");
+        }
+    }
+
+    public void onStartButtonClick() {
+        if (questions.isEmpty()) {
+            System.out.println("Need to load questions!");
+        } else {
+            System.out.println("Start button pressed!");
+        }
+    }
+
+    public void setPrimaryStage(Stage primaryStage) {
+        this.primaryStage = primaryStage;
     }
 }
