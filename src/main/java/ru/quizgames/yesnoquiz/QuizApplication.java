a42f1e4bbe87f05eb56b1e2578ab4a2e2c714949
aleksandr <felleet@gmail.com>
2025-03-23 23:43:56 +0300

    [m] изменил название приложения

--- src/main/java/ru/quizgames/yesnoquiz/QuizApplication.java
+++ src/main/java/ru/quizgames/yesnoquiz/QuizApplication.java
@@ -12,7 +12,7 @@ public class QuizApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(QuizApplication.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Викторина");
         stage.setScene(scene);
         stage.show();
     }
