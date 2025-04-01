3a982b8bf4b3b432f922cbe5fd54cda03e4b3fb5
aleksandr <felleet@gmail.com>
2025-04-01 20:37:38 +0300

    [+] релиз версии 1.1 [+] добавил фон

--- src/main/java/ru/quizgames/yesnoquiz/QuizApplication.java
+++ src/main/java/ru/quizgames/yesnoquiz/QuizApplication.java
@@ -4,22 +4,33 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 public class QuizApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        Background background;
+        try (InputStream inputStream = QuizApplication.class.getResourceAsStream("background.jpg")) {
+            Image image = new Image(inputStream);
+            BackgroundSize size = new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, true);
+            background = new Background(new BackgroundImage(image, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, size));
+        }
+
         FXMLLoader fxmlLoader = new FXMLLoader(QuizApplication.class.getResource("main-view.fxml"));
         Pane loadQuestionsView = fxmlLoader.load();
+        loadQuestionsView.setBackground(background);
         Scene scene = new Scene(loadQuestionsView, 620, 340);
         LoadQuestionsController loadQuestionsController = fxmlLoader.getController();
         loadQuestionsController.setPrimaryStage(stage);
         FXMLLoader quizLoader = new FXMLLoader(QuizApplication.class.getResource("quiz-view.fxml"));
         Pane quizView = quizLoader.load();
+        quizView.setBackground(background);
         QuizController quizController = quizLoader.getController();
         loadQuestionsController.setOnStartCallback(questions -> {
             scene.setRoot(quizView);
@@ -32,6 +43,7 @@ public class QuizApplication extends Application {
                 scene.setRoot(loadQuestionsView);
             }
         });
+
         stage.setTitle("Викторина");
         stage.setScene(scene);
         stage.show();
